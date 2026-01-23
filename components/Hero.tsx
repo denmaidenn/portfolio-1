29bfafe0c13ca9aad5b8bfd7a283584473eda308
Raffa Danendra <143143834+denmaidenn@users.noreply.github.com>
2026-01-24 00:32:14 +0700

    menyeusaikan project experience yang pernah saya kerjakan

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,8 +42,8 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+          <p className="tracking-widest text-sm md:text-base lg:text-lg text-center text-blue-100 max-w-2xl">
+            Hi! I&apos;m Raffa Danendra Pramono
           </p>
 
           {/**
@@ -52,12 +52,12 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Software Engineering Student at IPB University"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            <span className="text-purple">Backend Developer</span> on a partner project, responsible for developing <span className="text-purple">CRUD functionalities</span>, designing <span className="text-purple">REST APIs</span>, and managing data to support application requirements.
           </p>
 
           <a href="#about">
