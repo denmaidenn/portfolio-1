2e4cf10d62792c127227e31c719a71450b4f4f74
Raffa Danendra <143143834+denmaidenn@users.noreply.github.com>
2026-01-27 16:58:36 +0700

    tambahin gambar

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "/laptop.png",
     spareImg: "",
   },
   {
@@ -54,7 +54,7 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
+    img: "/db.png",
     spareImg: "/grid.svg",
   },
   {
